04e3bbad479bdccb7ea19b3fad31122179876308
Alin242003 <alinfilimon2003@gmail.com>
2024-02-15 19:35:13 +0100

    Update Program.cs

--- Progetto Saluber 2/Program.cs
+++ Progetto Saluber 2/Program.cs
@@ -9,7 +9,7 @@ class Program
     {
         // Imposta l'URL del web service
         string url = "https://clienti.saluber.it/api/stock/getLowAvailableItems";
-
+        ProvaGit
         // Esegui la chiamata al web service
         using (HttpClient client = new HttpClient())
         {
